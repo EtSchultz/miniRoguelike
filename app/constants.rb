be13700dc3b84685b32ac253a88363e43ac21067
EtSchultz <eth.schultz31@gmail.com>
2020-09-03 22:00:18 -0500

    added map mechanics

--- app/constants.rb
+++ app/constants.rb
@@ -3,11 +3,18 @@ GAME_BORDER_SPRITE = 'sprites/game_border.png'
 
 ROOM_BORDER_SPRITE = 'sprites/map_tile.png'
 PLAYER_STANDING_SPRITE_PATH = 'sprites/player_standing.png'
+PLAYER_LEFT_LEG_DOWN_SPRITE_PATH = 'sprites/player_move_0.png'
+PLAYER_RIGHT_LEG_DOWN_SPRITE_PATH = 'sprites/player_move_1.png'
 PLAYER_SPRITE_SHEET_PATH = 'sprites/player_sheet.png'
 
-#PLAYER_SP
+QUESTION_MARK_SPRITE_PATH = 'sprites/question_mark.png'
+
+PLAYER_SPRITE_X = 576
+PLAYER_SPRITE_Y = 268
 PLAYER_SPRITE_W = 128
 PLAYER_SPRITE_H = 128
+ROOM_BORDER_SPRITE_W = 144
+ROOM_BORDER_SPRITE_H = 144
 PLAYER_SPEED_LIMIT = 5
 PLAYER_ACCEL = 0.5
 PLAYER_FRICT = 0.9
@@ -48,7 +55,7 @@ def game_border_sprite
   }
 end
 
-def player_sprite
+def player_default_sprite
   {
     x: 576,
     y: 296,
@@ -60,10 +67,20 @@ end
 
 def room_border_sprite
   {
-    x: 568,
+    x: 568, #this will need to change to a variable
     y: 288,
-    w: PLAYER_SPRITE_W+16, 
-    h: PLAYER_SPRITE_H+16,
+    w: ROOM_BORDER_SPRITE_W, 
+    h: ROOM_BORDER_SPRITE_H,
     path: ROOM_BORDER_SPRITE
+  }
+end
+
+def question_mark_sprite
+  {
+    x: 568, #this will need to change to a variable
+    y: 432,
+    w: ROOM_BORDER_SPRITE_W, 
+    h: ROOM_BORDER_SPRITE_H,
+    path: QUESTION_MARK_SPRITE_PATH
   }
 end
--- app/main.rb
+++ app/main.rb
@@ -1,18 +1,14 @@
 require 'app/constants.rb'
 
 class Player
-  attr_accessor :pos, :sprite, :inv, :hp#, :last_step
+  attr_accessor :pos, :sprite, :inv, :hp, :last_step
 
-  def initilize(pos)
-    @pos = pos
+  def initilize
+    @pos = 0
     @inv = ["Fist", "Fist"]
     @hp = 100
-    @sprite = player_sprite
-    #@last_step = 
-  end
-
-  def sprite
-    @sprite
+    @sprite = player_default_sprite
+    @last_step = 1
   end
   
 end
@@ -20,10 +16,10 @@ end
 class Item
   attr_accessor :name, :dmg, :type
 
-  def initialize(seed, level)
-   # @type = gen_type(seed, level)
-   # @name = gen_name(seed, level)
-    #@dmg = gen_dmg(seed, level)
+  def initialize(name, type, dmg)
+    @type = type
+    @name = name
+    @dmg = dmg
   end
 
   def use_fist
@@ -41,6 +37,14 @@ class Item
   #end
 end
 
+class Enemy
+  attr_accessor :name, :type, :item, :dmg, :level
+
+  def initialize(name)
+    @name = name
+  end
+end
+
 class Room
   attr_accessor :pos, :enemy, :item, :is_open, :is_starting, :is_end
 
@@ -52,13 +56,25 @@ class Room
     @is_end = is_end
     @is_open = false
   end
+
+  def class
+    Room
+  end
+
 end
 
 class Game
-  attr_accessor :player, :args, :state, :inputs, :outputs, :grid, :seed
+  attr_accessor :player, :args, :state, :rooms, :inputs, :outputs, :grid, :seed, :map_generated
 
+  def initialize
+    @rooms ||= []
+    load_example_map
+    @player ||= Player.new
+    place_player_starting
+  end
+  
   def tick
-    defaults
+    #trace!
     render_canvas
     #render_map
     render_player
@@ -66,31 +82,32 @@ class Game
     #calc_canvas
     #input_move
     #input_click_map
-  end
-
-  def defaults
-
-    state.map  ||= EXAMPLE_MAP #[]
-    state.level ||= 1
-    #state.rooms   ||= []
-
+    draw_adjacent_rooms
   end
 
   def render_player
-    outputs.sprites << player_sprite
+    outputs.sprites << player_default_sprite
   end
   
   def render_canvas
     outputs.background_color = [0,0,0]
     outputs.sprites << game_border_sprite
+
+
+
     outputs.sprites << room_border_sprite
+
   end
 
-  
-  
   #def gen_map
 
   #end
+
+  def roll_item
+    intseed = @seed.to_i
+
+    Item.new(NAMES[0], TYPES[0], 10)
+  end
   
   def gen_seed
     chars = "0123456789"
@@ -107,9 +124,50 @@ class Game
     seed = str
   end
 
-  #def input_move
+  def load_example_map
+    i = 0
+    for room_num in EXAMPLE_MAP
+      if room_num != 0
+        #puts room_num
+        @rooms << Room.new(i,Enemy.new("nil"),roll_item, room_num == 2, room_num == 3)
+      end
+      i+=1
+    end
+    true
+  end
+
+  def place_player_starting
+    for room in @rooms
+      puts room.pos
+      if room.is_starting
+        @player.pos = room.pos
+        return
+      end
+    end
+  end
+
+  def draw_adjacent_rooms
+    #north
+    if @rooms.detect { |r| r.pos == (@player.pos+10)} != nil
+      outputs.sprites << [576, 432, PLAYER_SPRITE_W,PLAYER_SPRITE_H, QUESTION_MARK_SPRITE_PATH]
+    end
+    #south
+    if @rooms.detect { |r| r.pos == (@player.pos-10)} != nil
+      outputs.sprites << [576, 160, PLAYER_SPRITE_W,PLAYER_SPRITE_H, QUESTION_MARK_SPRITE_PATH]
+    end
+    #east
+    if @rooms.detect { |r| r.pos == (@player.pos+1)} != nil
+      outputs.sprites << [712, 296, PLAYER_SPRITE_W,PLAYER_SPRITE_H, QUESTION_MARK_SPRITE_PATH]
+    end
+    #west
+    if @rooms.detect { |r| r.pos == (@player.pos-1)} != nil
+      outputs.sprites << [440, 296, PLAYER_SPRITE_W,PLAYER_SPRITE_H, QUESTION_MARK_SPRITE_PATH]
+    end
+  end
     
-  #end
+  def input_move
+    
+  end
 end
 
 $game = Game.new
@@ -118,10 +176,10 @@ def tick args
   $game.seed ||= $game.gen_seed
   
   #$game.args    = args
-  $game.state   = args.state
+  #$game.state   = args.state
   #$game.inputs  = args.inputs
   $game.outputs = args.outputs
   #$game.grid    = args.grid
   $game.tick
-  args.outputs.labels << [100,600,$game.seed,1,1,255,255,255,255]
+  args.outputs.labels << [256,512,$game.seed,4,1,255,255,255,255, 'fonts/PICO-8 mono.ttf']
 end
